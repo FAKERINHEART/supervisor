a871ba249cf2d4c899d383c70a8f7431caaf0b73
Yajun Ge <137328237@qq.com>
2016-08-25 17:19:14 +0800

    启动时检查依赖否则起不来

--- supervisor/process.py
+++ supervisor/process.py
@@ -49,6 +49,7 @@ class Subprocess:
     exitstatus = None # status attached to dead process by finsh()
     spawnerr = None # error message attached by spawn() if any
     group = None # ProcessGroup instance if process is in the group
+    _can_spawn = True
 
     def __init__(self, config):
         """Constructor.
@@ -182,12 +183,24 @@ class Subprocess:
         self.spawnerr = msg
         self.config.options.logger.info("spawnerr: %s" % msg)
 
+    def can_spawn(self):
+        return self._can_spawn
+
+    def set_can_spawn(self):
+        self._can_spawn = True
+
     def spawn(self):
         """Start the subprocess.  It must not be running already.
+        且这个子进程的所有依赖都存在且都在RUNNING
 
         Return the process id.  If the fork() call fails, return None.
         """
         options = self.config.options
+
+        if not self.can_spawn():
+            msg = 'process %r cannot start - its owned group %r depends on processes which have not been started all yet: %r' % (self.config.name, self.group.config.name, self.config.dependson)
+            options.logger.warn(msg)
+            return
 
         if self.pid:
             msg = 'process %r already running' % self.config.name
